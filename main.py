75e185f9784e1204090dc3ae589becdff6215d19
michelbrik <brikmichel@gmail.com>
2023-04-30 17:59:46 -0300

    Adicionado teste para checar se o wifi esta funcionando.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import random
+import subprocess
 
-print(random.randint(0, 10))
+# Test if WIFI is on.
+ps = subprocess.Popen(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+try:
+    output = subprocess.check_output(('grep', 'ESSID'), stdin=ps.stdout)
+    print(output)
+except subprocess.CalledProcessError:
+    # grep did not match any lines
+    print("No wireless networks connected")
