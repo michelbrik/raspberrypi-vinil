0a05e8fd9171e67478236e9842cc8c1f46915020
michelbrik <brikmichel@gmail.com>
2023-04-30 18:17:36 -0300

    Acender luz se o wifi estiver funcionando.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import subprocess
 ps = subprocess.Popen(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 try:
     output = subprocess.check_output(('grep', 'ESSID'), stdin=ps.stdout)
-    print(output)
+    print("Acender luz verde.")
 except subprocess.CalledProcessError:
     # grep did not match any lines
-    print("No wireless networks connected")
+    print("Acender luz vermelha.")
